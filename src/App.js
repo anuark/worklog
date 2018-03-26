65d7fd2667b74c18d2f046e72ba98d743eb79361
Anuar Kilgore <jaicof@gmail.com>
2018-03-25 21:28:04 -0600

    Added admin-on-rest

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// in app.js
+import React from 'react';
+import { simpleRestClient, Admin, Resource } from 'admin-on-rest';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { PostList, PostEdit, PostCreate, PostIcon } from './posts';
+
+const App = () => (
+    <Admin restClient={simpleRestClient('http://localhost:3000')}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    </Admin>
+);
 
 export default App;
